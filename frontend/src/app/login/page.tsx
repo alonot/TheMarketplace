64d9225aa866652d5e1aed3fbe417974ea2a42d3
crypticks <kevinsalinskevin@gmail.com>
2025-04-11 20:34:04 +0530

    New page and CSS

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,30 @@
+import './page.css';
 export default function Page() {
-    return <h1>Hello Next.js!</h1>
+    return (
+        <div>
+
+            <div className="title">
+                <p id="title">YACC MARKETPLACE</p>
+            </div>
+            <div className = "body">
+                
+            </div>
+            <div id = "textboxes">    
+                <div className="entryarea" id = "emailentryarea">
+                    <input type = "text" className='textboxes' id = "emailbox" required />
+                    <div className="labelline">E-Mail</div>
+                    <br/>
+                    
+                </div>
+                <div className="entryarea" id = "passentryarea">
+                    <input type = "password" className='textboxes' id = "passbox" required />
+                    <div className="labelline">Password</div>
+                </div>
+                <div className="registertext">
+                    <p id = "registertext">New here? <a href="../register">Register Now</a></p>
+                </div>
+            </div>
+        </div>
+    )
+    
 }
